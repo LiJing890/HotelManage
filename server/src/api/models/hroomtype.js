7278ab99197f3a1c93a7bbc5ac913175abd92099
lijing <lij@all580.com>
2016-09-28 22:27:19 +0800

    房型设置

--- server/src/api/models/hroomtype.js
+++ server/src/api/models/hroomtype.js
@@ -19,6 +19,9 @@ export default class extends BaseModel {
         id: {type: 'integer', primaryKey: true, autoIncrement: true},//Id
         rtname: {type: 'string'},//房型名称
         rtcode: {type: 'string'},//房型编码
+        rtconfig:{type: 'string'},//配置名称
+        rtconfigids:{type: 'string'},//配置id
+        rtconfignum:{type: 'string'},//配置数量
         pricehour: {type: 'float'},//单价_小时
         priceday: {type: 'float'},//单价_天
         remark:{type:'string'}// 备注
