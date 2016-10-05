7504aaa28a6a9a8b5967a5f224f9e5fbc4a8a88d
lijing <1366596618@qq.com>
2016-10-05 12:00:48 +0800

    结算设置修改

--- server/src/api/models/hdiscount.js
+++ server/src/api/models/hdiscount.js
@@ -3,7 +3,6 @@
  *
  * @description :: 客房优惠设置表信息表
  *
- * Created by libinqi on 2016/7/3.
  */
 
 import {BaseModel} from './BaseModels/BaseModel';
@@ -15,8 +14,11 @@ export default class extends BaseModel {
 
   static tableName = 'hdiscount';
   static attributes = {
-        id: {type: 'integer', primaryKey: true, autoIncrement: true},//车位Id
-        housceamt: {type: 'float'},//客房优惠
-        htime: {type: 'datetime'}//客房结算时间
+        id: {type: 'integer', primaryKey: true, autoIncrement: true},//Id
+        hbegintime:{type: 'integer'},//入住时间
+        hendtime:{type: 'integer'},//退房时间
+        hmendtime:{type: 'integer'},//最晚退房时间
+        hmhour: {type: 'integer'},//最大钟点房时间
+        housceamt: {type: 'float'}//客房优惠
     }
 };
